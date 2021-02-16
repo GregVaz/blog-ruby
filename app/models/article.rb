d68f0cb6884ba087d836996dbcbdb2b03b757bab
Gregorio <gregoriovazya@gmail.com>
2021-02-15 19:34:32 -0700

    Validations and displaying errores messages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  #this means that the title value must contain at least one non-whitespace character
+  validates :title, presence: true
+  #it declares that the body value must be at least 10 characters long
+  validates :body, presence: true, length: { minimum: 10 }
 end
