793a7789d37b9d0392514b7cfc28d4d2aafa3f42
Gregorio <gregoriovazya@gmail.com>
2021-02-15 18:59:55 -0700

    Resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  #The resources method maps all of the conventional routes for a collection of resources, such as articles.
+  resources :articles
 end
