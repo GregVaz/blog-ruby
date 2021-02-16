562d0035938a4f462acc183ba60c5d2911f34de7
Gregorio <gregoriovazya@gmail.com>
2021-02-15 19:07:33 -0700

    CRUD Create controller methods new and create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = ARticle.new(article: "...", body: "...")
+
+    if @article.save
+      #redirect_to will cause the broser to maje a new request
+      redirect_to @article
+    else
+      #render renders thr specified view for the current request
+      render :new
+    end
+  end
 end
